58c31b38ea7f82ce87bae2468eabf733cbcb0661
brandon kudo and kyle ebersole <brandon@>
2017-09-12 11:53:46 -0700

    Add hidden-list class to index.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,58 @@
 $(document).ready(function(){
 
+$("#li1").click(function() {
+  $(".hide").slideToggle();
+  $(".show").slideToggle();
+});
 
+$("#li2").click(function() {
+  $(".hide").slideToggle();
+  $(".show").slideToggle();
+});
 
+$("#li3").click(function() {
+  $(".hide").slideToggle();
+  $(".show").slideToggle();
+});
 
+$("#li4").click(function() {
+  $(".hide").slideToggle();
+  $(".show").slideToggle();
+});
 
+$("#li5").click(function() {
+  $(".hide").slideToggle();
+  $(".show").slideToggle();
+});
 
+$("#li6").click(function() {
+  $(".hide").slideToggle();
+  $(".show").slideToggle();
+});
 
+$("#li7").click(function() {
+  $(".hide").slideToggle();
+  $(".show").slideToggle();
+});
 
+$("#li8").click(function() {
+  $(".hide").slideToggle();
+  $(".show").slideToggle();
+});
+
+$("#li9").click(function() {
+  $(".hide").slideToggle();
+  $(".show").slideToggle();
+});
+
+$("#li10").click(function() {
+  $(".hide").slideToggle();
+  $(".show").slideToggle();
+});
+
+$('li').click(function(){
+  $(this).next('.hidden-list').toggle();
+});
 
 
 
